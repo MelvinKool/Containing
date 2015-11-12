94007a623ca0658c0fc0a95ed7b88ad8de0b6e95
Jeffrey <jeffrey@ubuntu.nhl.nl>
2015-11-12 12:14:10 +0100

    fuck jmonkey

--- Simulator/src/Simulator/Main.java
+++ Simulator/src/Simulator/Main.java
@@ -3,15 +3,10 @@ package Simulator;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 
-/**
- * test
- * @author normenhansen
- */
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
@@ -33,11 +28,11 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
+        
     }
 }
