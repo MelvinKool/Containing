42e8d2665813032756e24ee5d5434a42c1cac83c
Jeffrey <jeffrey@ubuntu.nhl.nl>
2015-11-16 13:40:58 +0100

    merge jeffrey into develop

--- Simulator/src/Simulator/Main.java
+++ Simulator/src/Simulator/Main.java
@@ -7,39 +7,20 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 
-<<<<<<< HEAD
-/**
- * test
- * @author normenhansen
- */
-public class Main extends SimpleApplication {
-
-    public static void main(String[] args) {
-=======
 public class Main extends SimpleApplication
 {
-    
     Object AGV = new Object();
 
     public static void main(String[] args)
     {
->>>>>>> 0112b1c6549aff58a63c7bddbf7373d1b5fffea9
         Main app = new Main();
         app.start();
     }
 
     @Override
-<<<<<<< HEAD
     public void simpleInitApp() {
         Box b = new Box(1, 1, 1);
         Geometry geom = new Geometry("Box", b);
-=======
-    public void simpleInitApp()
-    {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-        
->>>>>>> 0112b1c6549aff58a63c7bddbf7373d1b5fffea9
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", ColorRGBA.Blue);
@@ -49,15 +30,6 @@ public class Main extends SimpleApplication
     }
 
     @Override
-<<<<<<< HEAD
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
-    }
-
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //TODO: add render code
-=======
     public void simpleUpdate(float tpf)
     {
         //TODO Depending on wich way you're going (XYZ) 
@@ -84,6 +56,5 @@ public class Main extends SimpleApplication
         //The AGV always moves at top speed, because reasons
         float tijd = verplaatsing/snelheid;
         return tijd;
->>>>>>> 0112b1c6549aff58a63c7bddbf7373d1b5fffea9
     }
 }
