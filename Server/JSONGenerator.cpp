8eb1f7940eaa29361f446b1d78e1b1dfb6e86ebd
Melvin <melvinsido@msn.com>
2015-11-30 14:35:49 +0100

    Json data structure fix

--- Server/JSONGenerator.cpp
+++ Server/JSONGenerator.cpp
@@ -16,7 +16,7 @@ rapidjson::Document JSONGenerator::createJSONDocument()
 }
 
 //generates JSON for moving a vehicle
-string JSONGenerator::moveTo()
+string JSONGenerator::moveTo(vector<> nodes)
 {
 	// document is the root of a json message
 	rapidjson::Document document = createJSONDocument();
@@ -35,22 +35,22 @@ string JSONGenerator::moveTo()
 	rapidjson::Value node1(rapidjson::kObjectType);
 	node1.AddMember("X", 40, allocator);
 	node1.AddMember("Y", 70, allocator);
-	node1.AddMember("X", 50, allocator);
+	node1.AddMember("Z", 50, allocator);
 	route.AddMember("Node1",node1, allocator);
 	rapidjson::Value node2(rapidjson::kObjectType);
 	node2.AddMember("X", 40, allocator);
 	node2.AddMember("Y", 70, allocator);
-	node2.AddMember("X", 50, allocator);
+	node2.AddMember("Z", 50, allocator);
 	route.AddMember("Node2",node2, allocator);
 	rapidjson::Value node3(rapidjson::kObjectType);
 	node3.AddMember("X", 40, allocator);
 	node3.AddMember("Y", 70, allocator);
-	node3.AddMember("X", 50, allocator);
+	node3.AddMember("Z", 50, allocator);
 	route.AddMember("Node3",node3, allocator);
 	rapidjson::Value node4(rapidjson::kObjectType);
 	node4.AddMember("X", 40, allocator);
 	node4.AddMember("Y", 70, allocator);
-	node4.AddMember("X", 50, allocator);
+	node4.AddMember("Z", 50, allocator);
 	route.AddMember("Node4",node4, allocator);
 	document.AddMember("Route", route, allocator);
 	//	fromScratch["object"]["hello"] = "Yourname";
--- Server/main.cpp
+++ Server/main.cpp
@@ -4,7 +4,6 @@
 #include "server.h"
 
 using namespace std;
-
 int main(int argc, char* argv[])
 {
     cout << endl << "Containing Server." << endl;
